d1da8929523e89e4217dc80cade84eb92e0bbb2a
niceRAM <niceniceram@gmail.com>
2021-11-24 14:52:34 +0800

    chore: 修正文件名含有空格时 new post 的问题

--- hugo-new.bat
+++ hugo-new.bat
@@ -1,3 +1,5 @@
-set FILE_NAME=%date:~0,4%%date:~5,2%%date:~8,2%_%time:~0,2%%time:~3,2%
-hugo new .\content\posts\%FILE_NAME%.md
+set hour=00%time:~0,2%
+set hour=%hour: =%
+set FILE_NAME=%date:~0,4%%date:~5,2%%date:~8,2%_%hour:~-2%%time:~3,2%
+hugo new ".\content\posts\%FILE_NAME%.md"
 exit
